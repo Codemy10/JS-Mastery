10343290872880e3986ce3030bdd06b9ec4a9d08
Abdulrahman <abdulrahmanbeyioku@gmail.com>
2025-10-09 23:10:34 +0100

    Completed literals

--- 3. Literals.js
+++ 3. Literals.js
@@ -2,16 +2,54 @@ const arr = ["tolu", "david", "john"]; //array literals
 
 console.log(arr);
 
+// Extra commas in array literals
+
+const arr2 = ["tolu", "david", /*empty this is a good practice */, "john"]; //array literals with extra commas
+
+console.log(arr2);
+console.log(arr2.length); // output: 4
+console.log(arr2[2]); // output: undefined
+
+
+//object literals
+
 const obj = { 
     name: "tolu",
      age: 23
-}; //object literals
+}; 
 
-console.log(obj);
+// Enhanced object literals
 
-const str = "hello world"; //string literals
+// const obj2 = { 
+//     // __proto__: theprototypeObject, // setting the prototype
+//     handler, // shorthand for handler: handler
+//     toString() { // method definition shorthand
+//         // super calls
+//         return "This is " + super.toString();
+//     }
+// };
+
+
+// String Literals
+
+const str = "hello world";
 
 console.log(str);
+
+let str2 = "this text will go to a new line \n see?"; // escape sequence for new line
+
+console.log(str2);
+
+
+// Template literals
+
+let fullname = "tolu";
+let age = 23;
+let greeting = `Hello, my name is ${fullname} and i am ${age} years`
+
+console.log(greeting)
+
+
 
 const num = 42; //number literals
 
