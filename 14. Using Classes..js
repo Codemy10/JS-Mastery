6a01b3f9745cc840107b27f2312b4ea42c6c4ce9
Abdulrahman <codemywebinar@gmail.com>
2025-12-05 18:39:33 +0100

    Finally finished classes after wasting a lot of time

--- 14. Using Classes..js
+++ 14. Using Classes..js
@@ -8,7 +8,8 @@ class Products{
 
     displayProduct(){ //Instance methods
         console.log(`Name : ${this.name}`)
-        console.log(`Price : $${this.price.toFixed(2)} imported by ${this.importer}`)
+        console.log(`Price : $${this.price.toFixed(2)} 
+        imported by ${this.importer}`)
     }
 
     priceAfterTax(vat){
@@ -88,11 +89,11 @@ class Crypto{
     }
 
     get crypto(){
-        return `${this.name} is currntly at ${this.price}`
+        return `${this.name} is currently at ${this.price}`
     }
 
-    set crypto(newprice){
-        this.price = newprice;
+    set crypts(newPrice){
+        this.price = newPrice;
     }
 
 }
@@ -102,3 +103,84 @@ const crypto1 = new Crypto("btc", 100000, 10)
 crypto1.crypto = 120000
 console.log(crypto1.crypto)
 
+
+// Public Fields 
+
+class newClass {
+    luckyNumber = Math.random();
+}
+
+console.log(new newClass().luckyNumber.toFixed(2));
+console.log(new newClass().luckyNumber.toFixed(3));
+
+
+// Equivalent to using  a constructor
+
+class anotherNewClass {
+    constructor(){
+        this.luckyNumber = Math.random();
+    }
+}
+
+console.log(new anotherNewClass().luckyNumber)
+
+
+// Static properties
+
+class newColor {
+  static isValid(r, g, b) {
+    return r >= 0 && r <= 255 && g >= 0 && g <= 255 && b >= 0 && b <= 255;
+  }
+  static {
+    newColor.myStaticProperty = "foo";
+  }
+}
+
+console.log(newColor.isValid(255, 0, 0)) // true
+newColor.isValid(1000, 0, 0); // false
+
+// Static properties are similar to instances except that 
+// they are prefixed with static and they are not accessible from instances 
+
+console.log(new newColor().isValid)   // THis will be undefined 
+
+
+
+// Extend and Inheritance 
+
+
+class parentClass {
+    constructor(a,b,c){
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    static {
+        parentClass.value = "fyp";
+    }
+
+    static isVoid(){
+
+    }
+    simpleMethod(){
+        return `${this.a}`
+    }
+}
+
+
+class childClass extends parentClass{
+    constructor(a,b,c,d){
+        super(a,b,c);
+        this.d = d;
+    }
+
+    simpleMethod(){
+        return `${super.simpleMethod().toUpperCase()}`
+    }
+
+}
+
+const tester = new childClass("Ola","","","Bola")
+
+console.log(tester.simpleMethod())
